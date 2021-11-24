8b6428c62a1fa966767010e88b94d083824a8fba
Miguel H <miguelsachorta@gmail.com>
2021-11-24 19:28:36 +0000

    ok?

--- src/main/java/pt/unl/fct/scc/controller/TestController.java
+++ src/main/java/pt/unl/fct/scc/controller/TestController.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     @GetMapping
     public String version(){
-        return "version: 00005 - messages done";
+        return "version: 0001 - Horta's branch";
     }
 }
