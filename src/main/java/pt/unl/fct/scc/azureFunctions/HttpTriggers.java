40f02181f24ae56a96f3905cb9a69b7d1e25752f
Miguel H <miguelsachorta@gmail.com>
2021-11-25 19:20:53 +0000

    epah nem sei honestamente

--- src/main/java/pt/unl/fct/scc/azureFunctions/HttpTriggers.java
+++ src/main/java/pt/unl/fct/scc/azureFunctions/HttpTriggers.java
@@ -1,26 +1,37 @@
 package pt.unl.fct.scc.azureFunctions;
 
+import com.azure.cosmos.CosmosContainer;
 import com.microsoft.azure.functions.*;
 
 import com.microsoft.azure.functions.annotation.AuthorizationLevel;
 import com.microsoft.azure.functions.annotation.*;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import pt.unl.fct.scc.service.CosmosDBService;
 
 import java.util.Optional;
 
+@Service
 public class HttpTriggers {
 
-    @FunctionName("delete-channel")
+    CosmosContainer userContainer;
+
+    public HttpTriggers(CosmosDBService cosmosDBService){
+        this.userContainer = cosmosDBService.getContainer("Users");
+    }
+
+    @FunctionName("updateUsersAfterChannelDelete")
     public HttpResponseMessage updateUsersAfterChannelDelete(@HttpTrigger(name = "updateUsersAfterChannelDelete",
             methods = {HttpMethod.DELETE},
             authLevel = AuthorizationLevel.ANONYMOUS,
-            route = "/rest/channels/{id}")
-                                            HttpRequestMessage<Optional<String>> request,
-                                    @BindingName("id") String id,
-                                    final ExecutionContext context) {
+            route = "/rest/channels/{id}") HttpRequestMessage<Optional<String>> request,
+            @BindingName("id") String id,
+            final ExecutionContext context) {
 
         return request.createResponseBuilder(HttpStatus.OK).body(String.format("Updated users in deleted channel: %s", id)).build();
     }
 
+    @FunctionName("updateChannelsAfterUserDelete")
     public HttpResponseMessage updateChannelsAfterUserDelete(@HttpTrigger(name = "updateChannelsAfterUserDelete",
             methods = {HttpMethod.DELETE},
             authLevel = AuthorizationLevel.ANONYMOUS,
@@ -32,6 +43,7 @@ public class HttpTriggers {
         return request.createResponseBuilder(HttpStatus.OK).body(String.format("Updated channels with deleted user: %s", id)).build();
     }
 
+    @FunctionName("updateMessagesAfterUserDelete")
     public HttpResponseMessage updateMessagesAfterUserDelete(@HttpTrigger(name = "updateMessagesAfterUserDelete",
             methods = {HttpMethod.DELETE},
             authLevel = AuthorizationLevel.ANONYMOUS,
--- src/main/java/pt/unl/fct/scc/controller/TestController.java
+++ src/main/java/pt/unl/fct/scc/controller/TestController.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     @GetMapping
     public String version(){
-        return "version: 0003 - ok sq ler as praticas ajuda";
+        return "version: 0004 - lol";
     }
 }
