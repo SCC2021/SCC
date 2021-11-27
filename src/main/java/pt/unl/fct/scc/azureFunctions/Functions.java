36645b45754934840b454e4afcbbbee82b37d924
Miguel H <miguelsachorta@gmail.com>
2021-11-27 03:43:22 +0000

    Clean up

--- src/main/java/pt/unl/fct/scc/azureFunctions/Functions.java
+++ src/main/java/pt/unl/fct/scc/azureFunctions/Functions.java
@@ -3,7 +3,6 @@ package pt.unl.fct.scc.azureFunctions;
 import com.azure.cosmos.CosmosContainer;
 import com.microsoft.azure.functions.*;
 
-import org.springframework.cloud.function.adapter.azure.FunctionInvoker;
 
 import com.microsoft.azure.functions.annotation.AuthorizationLevel;
 import com.microsoft.azure.functions.annotation.*;
