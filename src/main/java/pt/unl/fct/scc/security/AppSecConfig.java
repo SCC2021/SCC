ede046f0361afedd8179e445e15fb4f6dcb560d0
Pedro Dimas <pedro.m.dimas@gmail.com>
2021-11-28 16:23:05 +0000

    exclude test from sec

--- src/main/java/pt/unl/fct/scc/security/AppSecConfig.java
+++ src/main/java/pt/unl/fct/scc/security/AppSecConfig.java
@@ -13,6 +13,8 @@ public class AppSecConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
+                .antMatchers("/test")
+                .permitAll()
                 .anyRequest()
                 .authenticated()
                 .and()
