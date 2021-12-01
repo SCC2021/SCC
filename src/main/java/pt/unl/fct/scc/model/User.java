9b69424dc3cfc75c50c823e158d4f2c264d16ef6
Pedro Dimas <pedro.m.dimas@gmail.com>
2021-12-01 18:22:48 +0000

    Arrays pa lists

--- src/main/java/pt/unl/fct/scc/model/User.java
+++ src/main/java/pt/unl/fct/scc/model/User.java
@@ -4,6 +4,8 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
@@ -12,5 +14,5 @@ public class User {
     private String name;
     private String pwd;
     private String photoId;
-    private String[] channelIds;
+    private List<String> channelIds;
 }
